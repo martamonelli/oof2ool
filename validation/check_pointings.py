6b675629db264b6ba266294724148df8fe0b778a
Marta Monelli <di38wul@cm4login1.cos.lrz.de>
2025-03-04 06:08:48 +0100

    plotting phi and psi as well

--- validation/check_pointings.py
+++ validation/check_pointings.py
@@ -14,27 +14,39 @@ plt.rcParams.update({
 
 base_path = '/dss/dssfs02/lwp-dss-0001/pn36hu/pn36hu-dss-0000/oof2ool/pointings'
 
+###
+
 file_path = base_path + '/005rpm_450/pnt_0A_00000.fits'
 hdul = fits.open(file_path)
 pnt = hdul[1].data
 
 theta_005_450 = pnt['THETA']
+phi_005_450 = pnt['PHI']
+psi_005_450 = pnt['PSI']
 
+###
 
 file_path = base_path + '/030rpm_375/pnt_0A_00000.fits'
 hdul = fits.open(file_path)
 pnt = hdul[1].data
 
 theta_030_375 = pnt['THETA']
+phi_030_375 = pnt['PHI']
+psi_030_375 = pnt['PSI']
 
+###
 
 file_path = base_path + '/100rpm_260/pnt_0A_00000.fits'
 hdul = fits.open(file_path)
 pnt = hdul[1].data
 
 theta_100_260 = pnt['THETA']
+phi_100_260 = pnt['PHI']
+psi_100_260 = pnt['PSI']
 
-nsamp = 2000
+###
+
+nsamp = 10000
 
 plt.plot(theta_005_450[:nsamp], label='0.05rpm and 45.0deg')
 plt.plot(theta_030_375[:nsamp], label='0.30rpm and 37.5deg')
@@ -43,4 +55,20 @@ plt.legend()
 plt.xlabel('samples')
 plt.ylabel('theta')
 plt.savefig('thetas.png')
+
+plt.plot(phi_005_450[:nsamp], label='0.05rpm and 45.0deg')
+plt.plot(phi_030_375[:nsamp], label='0.30rpm and 37.5deg')
+plt.plot(phi_100_260[:nsamp], label='1.00rpm and 26.0deg')
+plt.legend()
+plt.xlabel('samples')
+plt.ylabel('phi')
+plt.savefig('phis.png')
+
+plt.plot(psi_005_450[:nsamp], label='0.05rpm and 45.0deg')
+plt.plot(psi_030_375[:nsamp], label='0.30rpm and 37.5deg')
+plt.plot(psi_100_260[:nsamp], label='1.00rpm and 26.0deg')
+plt.legend()
+plt.xlabel('samples')
+plt.ylabel('psi')
+plt.savefig('psis.png')
 
