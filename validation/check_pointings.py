3e9692da4f1f4b825acd594c66ea29cd49ac6146
Marta Monelli <di38wul@cm4login1.cos.lrz.de>
2025-03-04 03:38:43 +0100

    starting to check the pointings

---
+++ validation/check_pointings.py
@@ -0,0 +1,47 @@
+import numpy as np
+import healpy as hp
+from scipy.optimize import curve_fit
+
+from astropy.io import fits
+from matplotlib import pyplot as plt
+
+'''
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif'
+})
+'''
+
+base_path = '/dss/dssfs02/lwp-dss-0001/pn36hu/pn36hu-dss-0000/oof2ool/pointings'
+
+file_path = base_path + '/005rpm_450/pnt_0A_00000.fits'
+hdul = fits.open(file_path)
+pnt = hdul[1].data
+
+theta_005_450 = pnt['THETA']
+
+
+file_path = base_path + '/030rpm_450/pnt_0A_00000.fits'
+hdul = fits.open(file_path)
+pnt = hdul[1].data
+
+theta_030_450 = pnt['THETA']
+
+
+file_path = base_path + '/030rpm_375/pnt_0A_00000.fits'
+hdul = fits.open(file_path)
+pnt = hdul[1].data
+
+theta_030_375 = pnt['THETA']
+
+nmax_sparse = 100
+ratio = int(30/5)
+
+samps = np.arange(nmax_sparse*ratio)
+samps_sparse = np.arange(nmax_sparse)*ratio
+
+plt.plot(samps_sparse, theta_005_450[:nmax_sparse], label='theta for 0.05rpm and 45deg, stretched')
+plt.plot(samps, theta_030_450[:nmax_sparse*ratio], label='theta for 0.30rpm and 45deg')
+plt.legend()
+plt.savefig('thetas.png')
+
